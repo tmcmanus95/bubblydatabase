34dee3b7d73b7f75509127029acead61bf56a0a9
tmcmanus95 <tmcmanus95@gmail.com>
2024-02-25 23:28:44 -0600

    initial client config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,39 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import BubblyWater from "./pages/BubblyWater.jsx";
 
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/me",
+        element: <Profile />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/:bubblyId",
+        element: <BubblyWater />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
