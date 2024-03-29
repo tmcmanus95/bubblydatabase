a023a5fa00b8da37f4e434cedca65d72ef962c93
tmcmanus95 <tmcmanus95@gmail.com>
2024-03-29 00:33:42 -0500

    login/signup stylings

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -48,7 +48,7 @@ const Signup = () => {
           </p>
         ) : (
           <form onSubmit={handleFormSubmit}>
-            <div className="mb-4">
+            <div className="mb-4 border-2 border-solid px-1 border-sky-300">
               <input
                 className="form-input w-full"
                 placeholder="Your username"
@@ -59,7 +59,7 @@ const Signup = () => {
               />
             </div>
 
-            <div className="mb-4">
+            <div className="mb-4 border-2 border-solid px-1 border-sky-300">
               <input
                 className="form-input w-full"
                 placeholder="Your email"
@@ -70,7 +70,7 @@ const Signup = () => {
               />
             </div>
 
-            <div className="mb-4">
+            <div className="mb-4 border-2 border-solid px-1 border-sky-300">
               <input
                 className="form-input w-full"
                 placeholder="******"
@@ -83,7 +83,7 @@ const Signup = () => {
 
             <div className="text-center">
               <button
-                className="btn btn-block btn-info"
+                className="bg-transparent hover:bg-blue-300 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-300 hover:border-transparent rounded"
                 style={{ cursor: "pointer" }}
                 type="submit"
               >
