13a3328dec3086f9a6c3db8c9f9db5baf7f8e27b
tmcmanus95 <tmcmanus95@gmail.com>
2024-04-07 16:29:44 -0500

    reformatted radio buttons

--- client/src/components/FlavorSearchBar.jsx
+++ client/src/components/FlavorSearchBar.jsx
@@ -59,40 +59,42 @@ export default function FlavorSearchBar() {
                 value={searchTerm}
                 onChange={handleInputChange}
               />
-              <div className="flex flex-col items-center">
-                {" "}
-                {/* Container for radio buttons */}
-                <div className="flex items-center mb-4">
-                  <input
-                    id="default-radio-1"
-                    type="radio"
-                    value=""
-                    name="default-radio"
-                    onClick={toggleCaffeineSearch}
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    htmlFor="default-radio-1"
-                    className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Caffeine
-                  </label>
-                </div>
-                <div className="flex items-center">
-                  <input
-                    id="default-radio-2"
-                    type="radio"
-                    value=""
-                    onClick={toggleCBDSearch}
-                    name="default-radio"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    htmlFor="default-radio-2"
-                    className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    CBD
-                  </label>
+              <div className="flex justify-center">
+                <div className="flex items-center py-3">
+                  {" "}
+                  {/* Container for radio buttons */}
+                  <div className="flex items-center mr-2">
+                    <input
+                      id="default-radio-1"
+                      type="radio"
+                      value=""
+                      name="default-radio"
+                      onClick={toggleCaffeineSearch}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor="default-radio-1"
+                      className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >
+                      Caffeine
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input
+                      id="default-radio-2"
+                      type="radio"
+                      value=""
+                      onClick={toggleCBDSearch}
+                      name="default-radio"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor="default-radio-2"
+                      className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >
+                      CBD
+                    </label>
+                  </div>
                 </div>
               </div>
 
