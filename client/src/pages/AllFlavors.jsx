121382ffc6d3c8338d94acec05c7c76c6ced1954
tmcmanus95 <tmcmanus95@gmail.com>
2024-04-10 19:45:25 -0500

    edited all flavors page

--- client/src/pages/AllFlavors.jsx
+++ client/src/pages/AllFlavors.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import flavors from "../assets/flavors";
+import { formatBrands } from "../../utils/formatBrands";
 export default function AllFlavors() {
   return (
     <div className="mt-20 mb-14 justify-center flex flex-wrap gap-4 md:gap-6 lg:gap-10">
@@ -10,7 +11,7 @@ export default function AllFlavors() {
           >
             <div className="flex items-center justify-center h-full p-2 md:p-3 lg:p-4">
               <h3 className="text-sm md:text-base lg:text-lg font-medium">
-                {flavor}
+                {formatBrands(flavor)}
               </h3>
             </div>
           </div>
