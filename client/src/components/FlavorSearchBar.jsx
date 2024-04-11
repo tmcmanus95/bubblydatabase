328983e0a83cd714cf708a64e1c99245870e7f34
tmcmanus95 <tmcmanus95@gmail.com>
2024-04-10 19:34:18 -0500

    re-config of cbd/caffeine location

--- client/src/components/FlavorSearchBar.jsx
+++ client/src/components/FlavorSearchBar.jsx
@@ -25,26 +25,6 @@ export default function FlavorSearchBar() {
       handleSelectFlavor(filteredFlavors[0]);
     }
   };
-  const toggleCBDSearch = () => {
-    if (caffeineSearch == true) {
-      setCaffeineSearch(false);
-    }
-    setCBDSearch(true);
-  };
-  const toggleCaffeineSearch = () => {
-    if (CBDSearch == true) {
-      setCBDSearch(false);
-    }
-    setCaffeineSearch(true);
-  };
-  const toggleSearchAll = () => {
-    if (CBDSearch == true) {
-      setCBDSearch(false);
-    } else if (caffeineSearch == true) {
-      setCaffeineSearch(false);
-    }
-    setSearchAll(true);
-  };
 
   const filteredFlavors = flavors.filter((flavor) =>
     flavor.toLowerCase().includes(searchTerm.toLowerCase())
@@ -64,58 +44,6 @@ export default function FlavorSearchBar() {
                 value={searchTerm}
                 onChange={handleInputChange}
               />
-              <div className="flex justify-center">
-                <div className="flex items-center pt-3">
-                  <div className="flex items-center mr-2">
-                    <input
-                      id="default-radio-1"
-                      type="radio"
-                      value=""
-                      onClick={toggleSearchAll}
-                      name="default-radio"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                      htmlFor="default-radio-1"
-                      className="ms-1 text-sm font-medium text-gray-900 "
-                    >
-                      All
-                    </label>
-                  </div>
-                  <div className="flex items-center mr-2">
-                    <input
-                      id="default-radio-2"
-                      type="radio"
-                      value=""
-                      onClick={toggleCBDSearch}
-                      name="default-radio"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                      htmlFor="default-radio-2"
-                      className="ms-1 text-sm font-medium text-gray-900 "
-                    >
-                      CBD
-                    </label>
-                  </div>
-                  <div className="flex items-center">
-                    <input
-                      id="default-radio-3"
-                      type="radio"
-                      value=""
-                      onClick={toggleCaffeineSearch}
-                      name="default-radio"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                      htmlFor="default-radio-2"
-                      className="ms-1 text-sm font-medium text-gray-900 "
-                    >
-                      Caffeine
-                    </label>
-                  </div>
-                </div>
-              </div>
 
               {searchTerm && (
                 <ul className="absolute mt-1 bg-white border border-gray-300 rounded z-10 left-1/2 transform -translate-x-1/2 w-full md:w-80">
@@ -134,11 +62,7 @@ export default function FlavorSearchBar() {
           </form>
         </section>
         {selectedFlavor ? (
-          <BubblyWaterList
-            searchTerm={selectedFlavor.toLowerCase()}
-            caffeineSearch={caffeineSearch}
-            CBDSearch={CBDSearch}
-          />
+          <BubblyWaterList searchTerm={selectedFlavor.toLowerCase()} />
         ) : (
           <BubblyWaterList
             caffeineSearch={caffeineSearch}
--- client/src/pages/TopByFlavor.jsx
+++ client/src/pages/TopByFlavor.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import BubblyWaterList from "../components/BubblyWaterList";
 import Logo from "../components/Logo";
+import FlavorSearchBar from "../components/FlavorSearchBar";
 export default function TopByFlavor() {
   const { flavor } = useParams();
   return (
