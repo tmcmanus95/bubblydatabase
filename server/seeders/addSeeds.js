2a4848bb7773427222786e595e04758da02aaea1
tmcmanus95 <tmcmanus95@gmail.com>
2024-07-14 21:35:29 -0500

    disabled review for unverified users

--- server/seeders/addSeeds.js
+++ server/seeders/addSeeds.js
@@ -1,13 +1,32 @@
 const db = require("../config/onRenderConnection");
+const fs = require("fs");
+const path = require("path");
+
 const { BubblyWater } = require("../models");
 const newSeeds = require("./newSeeds.json");
+const existingSeedsPath = path.join(__dirname, "bubblyWaterSeeds.json");
 
 db.once("open", async () => {
   try {
+    let existingSeeds = [];
+    if (fs.existsSync(existingSeedsPath)) {
+      const data = fs.readFileSync(existingSeedsPath, "utf-8");
+      existingSeeds = JSON.parse(data);
+    }
+
+    const combinedSeeds = [...existingSeeds, ...newSeeds];
+
+    fs.writeFileSync(
+      existingSeedsPath,
+      JSON.stringify(combinedSeeds, null, 2),
+      "utf-8"
+    );
+
     await BubblyWater.create(newSeeds);
-    await console.log("~Additional Seeds added ! 🌱 📈 ~");
+    console.log("~Additional Seeds added ! 🌱 📈 ~");
     process.exit(0);
   } catch (err) {
-    throw err;
+    console.error("Error adding seeds:", err);
+    process.exit(1);
   }
 });
