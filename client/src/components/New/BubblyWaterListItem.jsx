4cb0d2f5d31a205cc814bee9be2e2074718cd0fd
tmcmanus95 <tmcmanus95@gmail.com>
2025-09-22 19:25:32 -0500

    updated flavor margins

--- client/src/components/New/BubblyWaterListItem.jsx
+++ client/src/components/New/BubblyWaterListItem.jsx
@@ -9,7 +9,7 @@ export default function BubblyWaterListItem({ bubblyWater }) {
   return (
     <div className="flex flex-row justify-center">
       <div
-        className={`${capitalizedFlavors[0]}-border transition-shadow flex flex-row border-solid border-2 border-gray-300 w-full sm:w-2/3`}
+        className={`${capitalizedFlavors[0]}-border transition-shadow flex flex-row border-solid border-2 w-full sm:w-2/3`}
       >
         <div className="flex flex-col">
           <img
@@ -18,7 +18,7 @@ export default function BubblyWaterListItem({ bubblyWater }) {
             className="mx-auto lg:mx-0 lg:mb-0 object-cover w-40 h-52 dark:rounded-lg"
           />
           <button
-            className={`mt-4 ${capitalizedFlavors[0]}-border ${
+            className={`${capitalizedFlavors[0]}-border ${
               capitalizedFlavors.length > 1
                 ? `${capitalizedFlavors[1]}-shadow`
                 : `${capitalizedFlavors[0]}-shadow`
@@ -27,23 +27,23 @@ export default function BubblyWaterListItem({ bubblyWater }) {
             Buy
           </button>
         </div>
-        <div className="flex flex-col justify-center">
-          <div className="flex flex-row">
+        <div className="flex flex-col justify-between p-4 md:items-center">
+          <div className="flex sm:flex-row flex-col md:justify-between text-center">
             <div>{formatBrands(bubblyWater.brandName)}</div>
             <div>{bubblyWater.productName}</div>
           </div>
-          <div className="flex flex-wrap max-w-[14rem] justify-center ">
+          <div className="flex flex-wrap max-w-[14rem] gap-3 justify-center">
             {capitalizedFlavors.map((flavor, index) => (
               <Link to={`/flavors/${flavor.toLowerCase()}`} key={index}>
-                <span
-                  className={`${flavor} whitespace-nowrap text-center ${flavor}-shadow`}
-                >
+                <span className={`${flavor} text-center ${flavor}-shadow`}>
                   {flavor.replace(/-/g, " ")}
                 </span>
               </Link>
             ))}
           </div>
-          <div>Average Rating: {bubblyWater.averageRating.toFixed(2)}</div>
+          <div className="text-center">
+            Avg Rating: {bubblyWater.averageRating.toFixed(2)} / 5
+          </div>
           <div className="flex justify-center">
             <CustomColorRating
               flavor={capitalizedFlavors[0]}
